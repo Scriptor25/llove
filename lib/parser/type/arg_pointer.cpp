2bf2623de86bf366f51a59a5559dcd438e11b4f9
Scriptor25 <f.schreiber.2006@gmail.com>
2025-08-19 14:26:12 +0200

    removed unnecessary type parsing

--- lib/parser/type/arg_pointer.cpp
+++
@@ -1,9 +0,0 @@
-#include <llove/context.hpp>
-#include <llove/parser.hpp>
-
-llove::TypePtr llove::Parser::ParseArgPointerType()
-{
-    Expect(TokenType_Operator, "<...>");
-
-    return m_Context.GetPointer(false);
-}
--- lib/parser/type/base.cpp
+++ lib/parser/type/base.cpp
@@ -21,8 +21,5 @@ llove::TypePtr llove::Parser::ParseBaseType()
     if (At(TokenType_Symbol))
         return ParseNamedType();
 
-    if (At(TokenType_Operator, "<...>"))
-        return ParseArgPointerType();
-
     Error(m_Token.Loc, "unable to parse type from {} : '{}'", m_Token.Type, m_Token.Value);
 }
